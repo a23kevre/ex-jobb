ea1be42d8fe51f8e32ec4a7c046e0860fa9a440e
a23kevre <164879829+a23kevre@users.noreply.github.com>
2026-03-23 18:14:14 +0100

    Update MySQL
    La in fyra tabbeler till databsen för att testa om det kommer upp och att allt fungerar nu eftersom mina förra test var på fel port.

--- MySQL Local.session.sql
+++ MySQL Local.session.sql
@@ -1,2 +1,10 @@
 CREATE DATABASE test;
 USE test;
+
+CREATE TABLE testing(nummer DOUBLE, word VARCHAR(20));
+
+INSERT INTO testing VALUES(1, "nummer1");
+INSERT INTO testing VALUES(2, "nummer2");
+INSERT INTO testing VALUES(3, "nummer3");
+INSERT INTO testing VALUES(4, "nummer4");
+INSERT INTO testing VALUES(5, "nummer5");
