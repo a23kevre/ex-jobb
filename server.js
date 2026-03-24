58c96ca7cf7a750f2f416517d5ae425fc9b289f4
a23kevre <164879829+a23kevre@users.noreply.github.com>
2026-03-24 01:56:50 +0100

    Update server.js
    La till en filesystem variabel och aktiverade fs sen läser jag html filen jag skapade i förra committen för att visa upp innehållet på webben, tog även bort hello world texten

--- server.js
+++ server.js
@@ -1,12 +1,20 @@
 const  { createServer } = require('node:http');
+const fs = require('fs');
 
 const hostname = '127.0.0.1';
 const port = 3000;
 
 const server = createServer((req, res) => {
-    res.statusCode = 200;
-    res.setHeader('Content-Type', 'text/plain');
-    res.end('Hello World');
+    fs.readFile('index.html', (err, data) => {
+        if (err) {
+            res.statusCode = 500;
+            res.end('Error loading this file');
+            return;
+        }
+        res.statusCode = 200;
+        res.setHeader('Content-Type', 'text/html');
+        res.end(data);
+    });
 });
 
 server.listen(port, hostname, () => {
