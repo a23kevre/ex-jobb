3817d1ccfbfc8192c02dec8611c8fb33bbcac241
a23kevre <164879829+a23kevre@users.noreply.github.com>
2026-03-25 22:20:55 +0100

    Update mysqlfetch.js
    skapade en funktion som tar emot en parameter som skapas i server.js för att utföra en sökning  där sökningen är ordet som skickades in eller skrevs in i input elementet på html sidan och från url tas detta värde för att utföra sökningen till databasen. Sen skapade jag en utskrivning av resultatet genom att skriva ut raderna och deras värden.

--- mysqlfetch.js
+++ mysqlfetch.js
@@ -1,5 +1,32 @@
 let mysql = require('./mysqlconnection');
-mysql.query('select * from testing', (err, rows) => {
-    if (err) throw err;
-    console.log(rows);
-});
+
+function mysqlSearch(req, res, mysqlUrl) {
+
+    let searchWord = mysqlUrl.searchParams.get('query') || '';
+
+    mysql.query('SELECT * FROM testing WHERE word LIKE ?',[`%${searchWord}%`], (err, rows) => {
+        if (err) {
+            console.error(err);
+            res.statusCode = 500;
+            res.end('Database error');
+            return;
+        } 
+        
+        res.setHeader('Content-Type', 'text/html');
+
+        let html = `<h1>Sökresultat för: ${searchWord}</h1>`;
+
+        if (rows.length === 0) {
+            html += `<p>Inga resultat hittades</p>`;
+        }
+        else {
+            rows.forEach(row => {
+                html += `<p> word ${row.word}, nummer (${row.nummer})</p>`;
+                html += `<br></br>`;
+            });
+        }
+
+        res.end(html);
+    });
+}
+module.exports = mysqlSearch;
