ec1b763f0fe3e746486ae47ebfc8d4b7d2cc3189
a23kevre <164879829+a23kevre@users.noreply.github.com>
2026-03-25 22:25:00 +0100

    Update server.js
    Som sagt i förra commiten skapar jag en variablel som är min URL. Jag hämtar också modulen från den andra filen mysqlfetch.js för att kunna använda funktionen jag skapade i den filen.  Sen skapar jag ifsatser så servern vet vad som ska visas på vilken URL den är på. När URL:en slutar på /search så används funktionen mysqlSearch. Delade upp filerna såhär för att server.js inte ska ha all kod eftersom det kommer kodas med MongoDB också.

--- server.js
+++ server.js
@@ -1,22 +1,36 @@
 const  { createServer } = require('node:http');
 const fs = require('fs');
+const mysqlSearch = require('./mysqlfetch');
 
 const hostname = '127.0.0.1';
 const port = 3000;
 
 const server = createServer((req, res) => {
-    fs.readFile('index.html', (err, data) => {
-        if (err) {
-            res.statusCode = 500;
-            res.end('Error loading this file');
-            return;
-        }
-        res.statusCode = 200;
-        res.setHeader('Content-Type', 'text/html');
-        res.end(data);
-    });
+
+    const mysqlUrl = new URL(req.url, `http://${req.headers.host}`);
+
+    if (mysqlUrl.pathname === '/') {
+        fs.readFile('index.html', (err, data) => {
+            if (err) {
+                res.statusCode = 500;
+                res.end('Error loading this file');
+                return;
+            }
+            res.statusCode = 200;
+            res.setHeader('Content-Type', 'text/html');
+            res.end(data);
+        });
+    }
+    else if (mysqlUrl.pathname === '/search') {
+        mysqlSearch(req, res, mysqlUrl);
+    }
+    else {
+        res.statusCode = 404;
+        res.end('Not found');
+    }
+    
 });
 
 server.listen(port, hostname, () => {
-    console.log('Server running at https://${hostname}:${port}/');
+    console.log(`Server running at http://${hostname}:${port}/`);
 });
