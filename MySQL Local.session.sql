338b35cd83ec869c3c3793e2a666a92d8e0d03c6
a23kevre <164879829+a23kevre@users.noreply.github.com>
2026-03-27 19:19:04 +0100

    Update MySQL Local.session.sql
    La till en procedure som loopar ut 50 insert into i testing tabellen för att kunna testa med mätningsscriptet som jag håller på att skriva så datan är mer än någon millisekund.

--- MySQL Local.session.sql
+++ MySQL Local.session.sql
@@ -8,3 +8,19 @@ INSERT INTO testing VALUES(2, "nummer2");
 INSERT INTO testing VALUES(3, "nummer3");
 INSERT INTO testing VALUES(4, "nummer4");
 INSERT INTO testing VALUES(5, "nummer5");
+
+DELIMITER //
+
+CREATE PROCEDURE insert_numbers()
+BEGIN
+    DECLARE i INT DEFAULT 1;
+
+        WHILE i <= 50 DO
+            INSERT INTO testing VALUES(i, CONCAT('nummer', i));
+        SET i = i + 1;
+    END WHILE;
+END //
+
+DELIMITER ;
+
+CALL insert_numbers();
