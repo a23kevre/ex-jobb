1ac108a8434b6b0f416d4b3342a42f91421d0c25
a23kevre <164879829+a23kevre@users.noreply.github.com>
2026-03-29 19:52:10 +0200

    Update playground-1.mongodb.js
    Updaterade så det ser ut som while loopen jag skapade i SQL där 50 olika nummer finns i databasen för testing.

--- playground-1.mongodb.js
+++ playground-1.mongodb.js
@@ -1,11 +1,10 @@
 // Select the database to use.
 use('test');
 
-// Insert a few documents into the testing collection.
-db.getCollection('testing').insertMany([
-  { nummer: 1, word: "nummer1" },
-  { nummer: 2, word: "nummer2" },
-  { nummer: 3, word: "nummer3" },
-  { nummer: 4, word: "nummer4" },
-  { nummer: 5, word: "nummer5" }
-]);
+// Loop insert nummer1->nummer50
+for (let i = 1; i <= 50; i++) {
+  db.getCollection('testing').insertOne({
+    nummer: i,
+    word: "nummer" + i
+  });
+}
