c42eaacbf70820e929dd03c1a7f19a78ef64cb33
a23kevre <164879829+a23kevre@users.noreply.github.com>
2026-04-03 18:49:57 +0200

    Update mysqlbenchmark.js
    lade till så att benchmarken söker med 2 random bokstäver precis som i mongodb istället för nummer1-50 och den mäter 1000 gånger så det blir bra med data kanske ändrar till högre tal beror på.

--- mysqlbenchmark.js
+++ mysqlbenchmark.js
@@ -1,38 +1,62 @@
 const mysql = require('./mysqlconnection');
 const fs = require('fs');
+const { performance } = require("perf_hooks");
 
-const iterations = 50;
+const iterations = 1000;
 
-// array med nummer1->nummer50
+function randomWord(length) {
+    const chars = "abcdefghijklmnopqrstuvwxyz";
+    let result = "";
+
+    for (let i = 0; i < length; i++) {
+        result += chars.charAt(Math.floor(Math.random() * chars.length));
+    }
+    return result;
+}
+
 const words = [];
-for (let i = 1; i <= iterations; i++) {
-    words.push(`nummer${i}`);
+for (let i = 0; i < iterations; i++) {
+    words.push(randomWord(2));
 }
 
 let csv = "word,start,end,delta,result\n";
 
+function queryDatabase(word) {
+    return new Promise((resolve, reject) => {
+        mysql.query(
+            "SELECT * FROM testing WHERE word LIKE ?",
+            [`%${word}%`],
+            (err, rows) => {
+                if (err) reject(err);
+                else resolve(rows);
+            }
+        );
+    });
+}
+
 async function mysqlBench() {
 
     for (let i = 0; i < words.length; i++) {
+
         const word = words[i];
         const start = performance.now();
-        const rows = await new Promise((resolve, reject) => {
-            mysql.query('SELECT * FROM testing WHERE word LIKE ?', [`%${word}%`],(err, rows) => {
-                if (err) reject(err);
-                else resolve(rows);
-            });
-        });
+
+        const results = await queryDatabase(word);
 
         const end = performance.now();
         const delta = end - start;
-        const resultCount = rows.length;
+        const resultCount = results.length;
+
         console.log(
             `#${i + 1} | ${word} | ${delta.toFixed(2)} ms | results: ${resultCount}`
         );
+
         csv += `${word},${start},${end},${delta},${resultCount}\n`;
     }
-    // spara filen
-    fs.writeFileSync('mysqlbenchmark.csv', csv);
-    console.log("\n Finished! Data saved to mysqlbenchmark.csv");
+    //spara filen
+    fs.writeFileSync("mysqlbenchmark.csv", csv);
+    console.log("\nData saved to mysqlbenchmark.csv");
+
+    process.exit();
 }
 mysqlBench();
